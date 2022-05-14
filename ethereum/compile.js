891f74a160ee25674fd1d4ce952365270584eb8a
Eliel Nfinic <elielmathe@nfinic.com>
2022-05-14 19:34:02 +0200

    Adding smart contract compiler

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,14 +2,27 @@ const path = require("path");
 const solc = require("solc");
 const fs = require("fs-extra");
 
-
 const buildPath = path.resolve(__dirname,"build");
 fs.removeSync(buildPath);
 
 const landderPath = path.resolve(__dirname,"contracts","Landder.sol");
 const source = fs.readFileSync(landderPath,'utf8');
-const output = solc.compile(source,1).contracts;
-console.log(output);
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Landder': {content : source}
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts.Landder;
 
 fs.ensureDir(buildPath);
 for(let contract in output){
--- test/Landder.test.js
+++ test/Landder.test.js
@@ -4,3 +4,21 @@ const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
+const compiledLandder = require("../ethereum/build/Landder.json");
+const compiledLand = require("../ethereum/build/Land.json");
+
+let accounts;
+let landder;
+let landAddress;
+let land;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+});
+
+describe('Landder',async() =>{
+    it("starts running",() => {
+        assert.ok(accounts);
+    });
+});
